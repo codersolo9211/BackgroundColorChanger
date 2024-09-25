436ca8b37a8ef1639aa23143a9f9c06c0a364184
codersolo9211 <141203987+codersolo9211@users.noreply.github.com>
2024-09-25 08:44:48 +0530

    seventh commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// genereTE a random color
+
+const colorGenearator = function(){
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+    for (let i = 0; i<6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+let intervalId;
+const startChangingColor = function (){
+    intervalId = setInterval(changeBgColor,1000)
+    function changeBgColor(){
+        document.body.style.backgroundColor = colorGenearator();
+
+    }
+}
+
+const stopChangingColor = function() {
+    clearInterval(intervalId);
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor)
+document.querySelector('#stop').addEventListener('click', stopChangingColor)
